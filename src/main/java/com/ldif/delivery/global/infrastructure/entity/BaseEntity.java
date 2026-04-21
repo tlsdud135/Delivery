14c278a45f57ac24e49ef9976f0dac918f9353b2
rodongwan <rodongwan@burritowallet.com>
2026-04-21 17:31:41 +0900

    add AuditingEntityListener to BaseEntity

--- src/main/java/com/ldif/delivery/global/infrastructure/entity/BaseEntity.java
+++ src/main/java/com/ldif/delivery/global/infrastructure/entity/BaseEntity.java
@@ -8,12 +8,13 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
-@EntityListeners(AutoCloseable.class)
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     // 1. 생성 시간
