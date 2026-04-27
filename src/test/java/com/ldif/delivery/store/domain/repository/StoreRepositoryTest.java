708fc7e8c0e1958643651cd6502f6161f96c15c2
181108-cherry <dysy9799@naver.com>
2026-04-27 15:33:18 +0900

    test: store sevice 테스트 코드 작성

---
+++ src/test/java/com/ldif/delivery/store/domain/repository/StoreRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.ldif.delivery.store.domain.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StoreRepositoryTest {
+
+    @Test
+    void findByNameContainingAndDeletedAtIsNull() {
+    }
+}
